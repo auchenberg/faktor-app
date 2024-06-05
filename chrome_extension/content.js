4757d1d9834995e8bd7dfa974579c7db685f3f61
Kenneth Auchenberg <kenneth@auchenberg.dk>
2024-06-04 20:31:30 -0400

    Update chrome extension

--- chrome_extension/content.js
+++ chrome_extension/content.js
@@ -3,6 +3,12 @@ console.log('factor.contentscript.loaded');
 
 chrome.runtime.sendMessage({ event: 'factor.content.loaded' });
 
+
+// setTimeout(() => {
+//     showAutocomplete({ code: '1234' });
+// }, 1000);
+
+
 chrome.runtime.onMessage.addListener(
     function (request, sender, sendResponse) {
         console.log('factor.contentscript.request', request);
@@ -20,10 +26,10 @@ chrome.runtime.onMessage.addListener(
 function showAutocomplete(data) {
     let code = data.code;
 
-    console.log('factor.contentscript.showAutocomplete', data);
-
     // Find all input elements with autocomplete="one-time-code"
-    const inputElements = document.querySelectorAll('input[autocomplete="one-time-code"]');
+    const inputElements = document.querySelectorAll('input');
+
+    console.log('factor.contentscript.showAutocomplete', data, inputElements);
 
     // Iterate over each input element
     inputElements.forEach(function (inputElement) {
@@ -32,36 +38,57 @@ function showAutocomplete(data) {
         const inputRect = inputElement.getBoundingClientRect();
         // Create a div element
         const divElement = document.createElement('div');
-        divElement.classList.add('autho-notification')
+        divElement.classList.add('faktor-notification')
 
         // Set the position of the div element
         divElement.style.top = `${inputRect.bottom}px`;
         divElement.style.left = `${inputRect.left}px`;
-        divElement.onclick = onNotificationClicked.bind(this, code, divElement, inputElement);
 
         // Insert the HTML into the DOM
         divElement.innerHTML = `
-            <div class="autho-tooltip">
-                <div class="autho-icon"></div>
-                <div class="autho-content">
+            <div class="faktor-tooltip">
+                <div class="faktor-icon"></div>
+                <div class="faktor-content">
                     <p>Fill code ${code}<p>
-                    <p class="autho-source">From Messages</p>
+                    <p class="faktor-source">From Messages</p>
                 </div>
             </div>
         `;
 
-        // Insert the div element into the body
-        document.body.appendChild(divElement);
+        inputElement.onfocus = function () {
+            console.log('faktor.contentscript.onfocus', inputElement, divElement)
+
+            setTimeout(() => {
+                document.body.appendChild(divElement);
+            }, 50);
+        }
+
+        inputElement.onblur = function () {
+            console.log('faktor.contentscript.onblur', inputElement, divElement)
+
+            setTimeout(() => {
+                divElement.remove();
+            }, 200);
+        }
+
+        divElement.onclick = function onNotificationClicked(e) {
+            console.log('factor.contentscript.onClick', code);
+
+            // Fill out value
+            inputElement.value = '';
+            document.execCommand('insertText', false, code);
+
+            // Clean up
+            divElement.remove();
+            inputElement.onfocus = null;
+            inputElement.onblur = null;
+        }
+
+        // Check if inputElement is currently focused
+        if (document.activeElement === inputElement) {
+            inputElement.onfocus();
+        }
+
     });
 }
 
-function onNotificationClicked(code, targetBox, targetInput, e) {
-    console.log('factor.contentscript.onNotificationClicked', code);
-
-    targetInput.focus();
-    targetInput.value = '';
-
-    document.execCommand('insertText', false, code);
-
-    targetBox.remove();
-}
