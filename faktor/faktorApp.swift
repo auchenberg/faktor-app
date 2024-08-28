d6d8411b7e5a1c6ee9d80fa4ac88ba68e6c8f620
Kenneth Auchenberg <kenneth@auchenberg.dk>
2024-08-28 15:45:53 -0400

    Implement toggle of dock icon

--- faktor/faktorApp.swift
+++ faktor/faktorApp.swift
@@ -11,12 +11,14 @@ extension Defaults.Keys {
     static let settingShowNotifications = Key<Bool>("showNotifications", default: true)
     static let settingsEnableBrowserIntegration = Key<Bool>("enableBrowserIntegration", default: true)
     static let settingsShowWelcome = Key<Bool>("showWelcome", default: true)
+    static let settingsShowInDock = Key<Bool>("showInDock", default: false)
 }
 
 class AppDelegate: NSObject, NSApplicationDelegate {
     @Default(.settingShowNotifications) var settingShowNotifications
     @Default(.settingsEnableBrowserIntegration) var settingsEnableBrowserIntegration
     @Default(.settingsShowWelcome) var settingsShowWelcome
+    @Default(.settingsShowInDock) var settingsShowIndock
     var messageManager = MessageManager()
     var appStateManager = AppStateManager()
     var notificationManager: NotificationManager
@@ -29,6 +31,18 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         browserManager = BrowserManager(messageManager: messageManager)
         super.init()
     }
+    
+    func applicationWillFinishLaunching(_ notification: Notification) {
+        updateDockIconVisibility(isVisible: settingsShowIndock)
+        
+        // Set up observer for settings changes
+        Task {
+            for await value in Defaults.updates(.settingsShowInDock) {
+                self.updateDockIconVisibility(isVisible: value)
+            }
+        }
+
+     }
     
     func applicationDidFinishLaunching(_ notification: Notification) {
         
@@ -54,6 +68,12 @@ class AppDelegate: NSObject, NSApplicationDelegate {
             // Show welcome view
         }
     
+    }
+    
+    // Initial setup of dock icon visibility
+    func updateDockIconVisibility(isVisible: Bool = false) {
+        print("updateDockIconVisibility", isVisible)
+        NSApp.setActivationPolicy(isVisible ? .regular : .accessory)
     }
 }
 
