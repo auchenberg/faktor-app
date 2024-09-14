d96818ff0f8368ccca7d218f8aa34536eebfc6af
Kenneth Auchenberg <kenneth@auchenberg.dk>
2024-09-14 14:16:31 -0400

    Make types explicit

--- faktor/Extensions/Logger.swift
+++ faktor/Extensions/Logger.swift
@@ -2,8 +2,8 @@ import OSLog
 
 extension Logger {
     /// Using your bundle identifier is a great way to ensure a unique identifier.
-    private static var subsystem = Bundle.main.bundleIdentifier!
+    private static var subsystem: String = Bundle.main.bundleIdentifier!
 
     /// Logs the view cycles like a view that appeared.
-    static let core = Logger(subsystem: subsystem, category: "core")
+    static let core: Logger = Logger(subsystem: subsystem, category: "core")
 }
